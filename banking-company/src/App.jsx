49bbe5bb55fde3dcc8950d06742402e7dc97e22c
◇HemîD◇ <124708686+Hamid061002@users.noreply.github.com>
2023-12-10 01:16:04 +0330

    feat: welcome components created

--- banking-company/src/App.jsx
+++ banking-company/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import './assets/style.css'
 import Header from './components/header/Header.jsx'
 import './App.css'
+import Welcome from './components/welcome/Welcome'
 
 function App() {
    return (
-      <div>
+      <div className='bg-grey-10 absolute w-full pb-20 -z-50'>
+         <div className="bgHeader bg-no-repeat h-[500px] w-[800px] bg-contain absolute -left-20 -top-20 -z-40"></div>
          <Header />
+         <Welcome />
       </div>
    )
 }
--- banking-company/src/components/header/Header.jsx
+++ banking-company/src/components/header/Header.jsx
@@ -2,8 +2,7 @@ import logo from '../../../public/images/logos/YourBank.svg'
 import logoText from '../../../public/images/logos/YourBankText.svg'
 function Header() {
    return (
-      <header className="bg-grey-10 absolute w-full -z-20 h-screen">
-         <div className="bgHeader h-[500px] bg-no-repeat bg-contain absolute w-full -left-20 -top-20 -z-10"></div>
+      <header className="fixed w-full z-50">         
          <div className="flex justify-center py-[30px]">
             <div className="flex justify-between items-center w-[1280px] px-6 py-[14px] bg-grey-11 rounded-full text-white">
                <div className="flex-none flex gap-1 items-center">
@@ -22,7 +21,7 @@ function Header() {
                </div>
                <div className="flex-none flex items-center gap-[30px] text-lg LexendFontRegular">
                   <div className="leading-7 cursor-pointer transition-all duration-300 hover:-translate-y-1">Sign Up</div>
-                  <div className="leading-7 cursor-pointer w-[109px] text-center py-[14px] bg-green-60 hover:text-xl transition-all duration-300 text-grey-11 rounded-full">Login</div>
+                  <div className="leading-7 cursor-pointer w-[109px] text-center py-[14px] bg-green-60 hover:bg-[#22251B] hover:text-green-60 transition-all duration-300 text-grey-11 rounded-full">Login</div>
                </div>
             </div>
          </div>
