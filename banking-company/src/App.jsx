c7594e613a2db0e4b6027c30ae910993b710fa27
◇HemîD◇ <124708686+Hamid061002@users.noreply.github.com>
2023-12-13 18:08:42 +0330

    fix: fix some problems

--- banking-company/src/App.jsx
+++ banking-company/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import './assets/style.css'
-import Header from './components/header/Header.jsx'
 import './App.css'
-import Welcome from './components/welcome/Welcome'
 import Home from './pages/home/Home'
 
 function App() {
--- banking-company/src/pages/home/Home.jsx
+++ banking-company/src/pages/home/Home.jsx
@@ -4,20 +4,20 @@ import OurProducts from '../../components/our products/OurProducts'
 import UseCases from '../../components/use cases/UseCases'
 import Welcome from '../../components/welcome/Welcome'
 function Home() {
-    return (
-        <div className="flex justify-center bg-grey-10 absolute w-full -z-50 overflow-x-hidden">
-            <Header />
-            <div className="max-w-[1536px]">
-                <div className="bgHeader bg-no-repeat h-[300px] w-[500px] lg:h-[500px] lg:w-[800px] bg-contain fixed -left-10 -top-12 lg:-left-20 lg:-top-20 -z-40"></div>
-                <div className="flex flex-col gap-20 lg:gap-[120px]">
-                    <Welcome />
-                    <OurProducts />
-                    <UseCases />
-                    <Footer />
-                </div>
+   return (
+      <section className="flex justify-center bg-grey-10 absolute w-full -z-50 overflow-x-hidden">
+         <Header />
+         <div className="max-w-[1536px]">
+            <div className="bgHeader bg-no-repeat h-[300px] w-[500px] lg:h-[500px] lg:w-[800px] bg-contain fixed -left-10 -top-12 lg:-left-20 lg:-top-20 -z-40"></div>
+            <div className="flex flex-col gap-20 lg:gap-[120px]">
+               <Welcome />
+               <OurProducts />
+               <UseCases />
+               <Footer />
             </div>
-        </div>
-    )
+         </div>
+      </section>
+   )
 }
 
 export default Home
